512895570cedd4aaf82d4575fc58e6eefd5589c3
dc <dc@local>
2023-11-21 14:36:04 +0000

    exercises: part2b: 2.10
    Is this good style though, since it isn't moving the actual buisness logic?

--- exercises/part2/phonebook/src/App.jsx
+++ exercises/part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -42,23 +45,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        onNewNameChange={handleNameChange}
+        newNumber={newNumber}
+        onNewNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      {personsToShow.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+      <Persons persons={personsToShow} />
     </div>
   )
 }
