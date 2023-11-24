a46f210821ea673ec390657a9da300f3abdae2fd
dc <dc@local>
2023-11-24 11:49:38 +0000

    exercises: part2e: 2.19

--- exercises/part2/countries/src/components/CountriesList.jsx
+++ exercises/part2/countries/src/components/CountriesList.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
+import CountryInfo from './CountryInfo'
+
+const CountriesListItem = ({ country }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  return (
+    <div>
+      {country.name.common}
+      <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'hide' : 'show'}</button>
+      {showInfo && <CountryInfo country={country} />}
+    </div>
+  )
+}
+
 const CountriesList = ({ countries }) => {
   return (
-    <>{countries.map((country) => <div key={country.name.common}>{country.name.common}</div>)}</>
+    <>{countries.map((country) => <CountriesListItem key={country.name.common} country={country} />)}</>
   )
 }
 
