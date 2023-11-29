e41ca305113ef2d605a8a265bf5148767b0c7d24
dc <dc@local>
2023-11-29 08:19:42 +0000

    exercises: part3a: 3.8

--- exercises/part3/index.js
+++ exercises/part3/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+morgan.token('body', (request, response) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
