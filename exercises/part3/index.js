5d209ec724ba8796cb295ca340b62f456da151ea
dc <dc@local>
2023-11-29 08:07:38 +0000

    exercises: part3a: 3.2

--- exercises/part3/index.js
+++ exercises/part3/index.js
@@ -28,6 +28,10 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`Phonebook has info for ${persons.length} people<br /><br />${new Date().toString()}`)
+})
+
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
