6c77d23302dd4c63e68dd23cbb080387ca8c9d4c
dc <dc@local>
2023-12-01 08:58:04 +0000

    exercises: part3b: 3.11

--- exercises/part3/index.js
+++ exercises/part3/index.js
@@ -12,6 +12,7 @@ morgan.token('body', (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
   {
