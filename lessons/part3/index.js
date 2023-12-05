0ee6096ea8e0a2094efecb123d76bacda3ec0dc1
dc <dc@local>
2023-12-05 09:07:18 +0000

    lessons: part3d: validation

--- lessons/part3/index.js
+++ lessons/part3/index.js
@@ -22,7 +22,9 @@ const errorHandler = (error, request, response, next) => {
   console.error('---')
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformmated id' })
+    return response.status(400).json({ error: 'malformmated id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -64,37 +66,32 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then((updatedNote) => {
       response.json(updatedNote)
     })
     .catch((error) => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then((savedNote) => {
+      response.json(savedNote)
+    })
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
