c855ce37f4b63b263ca4bac6a2f15ec7f63d5ad9
dc <dc@local>
2023-12-09 09:27:30 +0000

    exercises: part4a: 4.5-4.7

--- exercises/part4/utils/list_helper.js
+++ exercises/part4/utils/list_helper.js
@@ -1,14 +1,45 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
 
 const totalLikes = (blogs) => {
-  return blogs
-    .map((blog) => blog.likes)
-    .reduce((sum, n) => sum + n, 0)
+  return _.sumBy(blogs, 'likes')
+}
+
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) return
+  const { title, author, likes } = _.maxBy(blogs, 'likes')
+  return { title, author, likes }
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return
+  const [author, numBlogs] = _(blogs)
+    .countBy('author')
+    .toPairs()
+    .maxBy(1)
+  return {
+    author,
+    blogs: numBlogs,
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return
+  const [author, likes] = _(blogs)
+    .groupBy('author')
+    .mapValues((value) => _(value).sumBy('likes'))
+    .toPairs()
+    .maxBy(1)
+  return { author, likes }
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes,
 }
