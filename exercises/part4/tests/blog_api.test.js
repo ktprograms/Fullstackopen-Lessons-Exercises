8d3857cf280c5e4be8cbeb86fe759579ef4a7b4d
dc <dc@local>
2023-12-18 07:36:27 +0000

    exercises: part4b: 4.13
    also refactor earlier exercises tests

--- exercises/part4/tests/blog_api.test.js
+++ exercises/part4/tests/blog_api.test.js
@@ -30,113 +30,143 @@ test('unique identifier property is named id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
-test('a valid note can be added', async () => {
-  const newBlog = {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map((b) => b.title)
-  expect(titles).toContain(
-    'Type wars'
-  )
-})
-
-describe('likes property defaults to 0 on new blog', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
-  })
-
-  test('test backend response', async () => {
+describe('adding blogs', () => {
+  test('succeeds with valid data', async () => {
     const newBlog = {
       title: 'Type wars',
       author: 'Robert C. Martin',
       url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
     }
 
-    const resultBlog = await api
+    await api
       .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    expect(resultBlog.body.likes).toBe(0)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map((b) => b.title)
+    expect(titles).toContain(
+      'Type wars'
+    )
   })
 
-  test('test database contents', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    }
+  describe('likes property defaults to 0 when undefined', () => {
+    beforeEach(async () => {
+      await Blog.deleteMany({})
+    })
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
+    test('test backend response', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      }
 
-    const blogsAtEnd = await helper.blogsInDb()
+      const resultBlog = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-    expect(blogsAtEnd[0].likes).toBe(0)
+      expect(resultBlog.body.likes).toBe(0)
+    })
+
+    test('test database contents', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd[0].likes).toBe(0)
+    })
+  })
+
+  describe('fails with status code 400 if data is invalid', () => {
+    test('without title', async () => {
+      const newBlog = {
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        author: 'Robert C. Martin',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('without url', async () => {
+      const newBlog = {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('without title or url', async () => {
+      const newBlog = {
+        author: 'Robert C. Martin',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
   })
 })
 
-describe('invalid blogs not added', () => {
-  test('without title', async () => {
-    const newBlog = {
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      author: 'Robert C. Martin',
-    }
+describe('deleting blogs', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
     await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
     const blogsAtEnd = await helper.blogsInDb()
 
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map((b) => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
   })
 
-  test('without url', async () => {
-    const newBlog = {
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-    }
+  test('fails with status code 400 if id is invalid', async () => {
+    const invalidId = '0'
 
     await api
-      .post('/api/blogs')
-      .send(newBlog)
+      .delete(`/api/blogs/${invalidId}`)
       .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-  })
-
-  test('without title or url', async () => {
-    const newBlog = {
-      author: 'Robert C. Martin',
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
 })
 
--- exercises/part4/tests/test_helper.js
+++ exercises/part4/tests/test_helper.js
@@ -38,6 +38,14 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON())
 }
 
+const nonExistingId = async () => {
+  const blog = new Blog({ title: 'willremovethissoon', url: 'willremovethissoon' })
+  await blog.save()
+  await blog.deleteOne()
+
+  return blog._id.toString()
+}
+
 module.exports = {
-  initialBlogs, blogsInDb,
+  initialBlogs, blogsInDb, nonExistingId,
 }
