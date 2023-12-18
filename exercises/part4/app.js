249b4b6cb2493711b0d4b61dafa062ff658c19bc
dc <dc@local>
2023-12-18 03:11:22 +0000

    exercises: part4b: 4.8

--- exercises/part4/app.js
+++ exercises/part4/app.js
@@ -1,11 +1,11 @@
+const config = require('./utils/config')
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 
-const mongoUrl = 'mongodb+srv://root:pw@qc4sgk8.coolify.home.test/bloglist?tls=false&authSource=admin'
-mongoose.connect(mongoUrl)
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
---
+++ exercises/part4/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (const blog of helper.initialBlogs) {
+    const blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('correct number of blogs returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
