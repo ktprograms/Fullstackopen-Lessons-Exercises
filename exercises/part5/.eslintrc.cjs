28e4a4855d9db59938b311d68e80afa4e4e80fa1
dc <dc@local>
2023-12-20 13:12:55 +0000

    exercises: part5a: 5.1

---
+++ exercises/part5/.eslintrc.cjs
@@ -0,0 +1,70 @@
+// enable this to sort module.exports.overrides
+/* eslint sort-keys: 2 */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'arrow-parens': 'error',
+    'arrow-spacing': [
+      'error',
+      {
+        'before': true,
+        'after': true,
+      },
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always-multiline',
+        'objects': 'always-multiline',
+        'imports': 'always-multiline',
+        'exports': 'always-multiline',
+        'functions': 'never',
+      },
+    ],
+    'eqeqeq': 'error',
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'no-console': 0,
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+    'semi': [
+      'error',
+      'never',
+    ],
+    'space-in-parens': [
+      'error',
+      'never',
+    ],
+    'space-infix-ops': 'error',
+  },
+}
