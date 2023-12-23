6ad66afb5b377694b8af0dca45dc550056aef51a
dc <dc@local>
2023-12-23 08:23:47 +0000

    exercises: part5b: 5.7

--- exercises/part5/src/components/Blog.jsx
+++ exercises/part5/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setDetailsVisible(!detailsVisible)}>
+        {detailsVisible ? 'hide' : 'view'}
+      </button>
+
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        likes {blog.likes} <button>like</button><br />
+        {blog.user.name}<br />
+      </div>
+    </div>
+  )
+}
 
 export default Blog
