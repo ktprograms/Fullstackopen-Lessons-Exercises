30ce275e8626ff4373ce3a9c75155252f61bf46d
dc <dc@local>
2023-12-23 04:34:20 +0000

    lessons: part5b: PropTypes

--- lessons/part5/src/components/LoginForm.jsx
+++ lessons/part5/src/components/LoginForm.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { useState } from 'react'
 
 const LoginForm = ({ onLogin }) => {
@@ -42,6 +43,10 @@ const LoginForm = ({ onLogin }) => {
       </form>
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  onLogin: PropTypes.func.isRequired,
 }
 
 export default LoginForm
