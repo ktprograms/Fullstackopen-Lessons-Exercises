4e2f32b6783fd9cacfa9d8d9e19e94df6bb08f05
dc <dc@local>
2023-12-26 05:23:07 +0000

    exercises: part5c: 5.15

--- exercises/part5/src/components/Blog.test.js
+++ exercises/part5/src/components/Blog.test.js
@@ -13,12 +13,10 @@ const blog = {
     name: 'kt',
   },
 }
-let container
-beforeEach(() => {
-  container = render(<Blog blog={blog} />).container
-})
 
 test('renders title and author, but not url or # of likes', () => {
+  const { container } = render(<Blog blog={blog} />)
+
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent(blog.title)
   expect(div).toHaveTextContent(blog.author)
@@ -31,10 +29,25 @@ test('renders title and author, but not url or # of likes', () => {
 })
 
 test('shows details when view button is clicked', async () => {
+  const { container } = render(<Blog blog={blog} />)
+
   const user = userEvent.setup()
   const button = screen.getByText('view')
   await user.click(button)
 
   const detailsDiv = container.querySelector('.blog__details')
   expect(detailsDiv).not.toHaveStyle({ display: 'none' })
+})
+
+test('like handler called for every click of like button', async () => {
+  const mockHandler = jest.fn()
+
+  render(<Blog blog={blog} onLikeBlog={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
