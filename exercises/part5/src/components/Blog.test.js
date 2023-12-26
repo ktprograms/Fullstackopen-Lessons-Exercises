ba824cfa747ca58bcd9c97df585ecf45302a6349
dc <dc@local>
2023-12-26 05:18:36 +0000

    exercises: part5c: 5.14

--- exercises/part5/src/components/Blog.test.js
+++ exercises/part5/src/components/Blog.test.js
@@ -1,21 +1,24 @@
 /* eslint sort-imports: off */
 import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  title: 'React patterns',
+  author: 'Michael Chan',
+  url: 'https://reactpatterns.com/',
+  likes: 7,
+  user: {
+    name: 'kt',
+  },
+}
+let container
+beforeEach(() => {
+  container = render(<Blog blog={blog} />).container
+})
+
 test('renders title and author, but not url or # of likes', () => {
-  const blog = {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    user: {
-      name: 'kt',
-    },
-  }
-
-  const { container } = render(<Blog blog={blog} />)
-
   const div = container.querySelector('.blog')
   expect(div).toHaveTextContent(blog.title)
   expect(div).toHaveTextContent(blog.author)
@@ -25,4 +28,13 @@ test('renders title and author, but not url or # of likes', () => {
   expect(detailsDiv).toHaveTextContent(blog.url)
   expect(detailsDiv).toHaveTextContent(blog.likes)
   expect(detailsDiv).toHaveTextContent(blog.user.name)
+})
+
+test('shows details when view button is clicked', async () => {
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const detailsDiv = container.querySelector('.blog__details')
+  expect(detailsDiv).not.toHaveStyle({ display: 'none' })
 })
