6e48d2603581b7b87fce9e02e5bee816979018ca
dc <dc@local>
2023-12-26 07:08:37 +0000

    exercises part5c: add coverage
    add tests to get full coverage of Blog component

--- exercises/part5/src/components/Blog.test.js
+++ exercises/part5/src/components/Blog.test.js
@@ -41,13 +41,50 @@ test('shows details when view button is clicked', async () => {
 
 test('like handler called for every click of like button', async () => {
   const mockHandler = jest.fn()
+  const user = userEvent.setup()
 
   render(<Blog blog={blog} onLikeBlog={mockHandler} />)
 
-  const user = userEvent.setup()
   const button = screen.getByText('like')
   await user.click(button)
   await user.click(button)
 
   expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
+describe('remove button', () => {
+  test('is not in document when showRemove=false', () => {
+    render(<Blog blog={blog} showRemove={false} />)
+
+    const element = screen.queryByText('delete')
+    expect(element).not.toBeInTheDocument()
+  })
+
+  describe('when clicked', () => {
+    test('handler called if user confirms', async () => {
+      jest.spyOn(globalThis, 'confirm').mockReturnValueOnce(true)
+      const mockHandler = jest.fn()
+      const user = userEvent.setup()
+
+      render(<Blog blog={blog} showRemove={true} onRemoveBlog={mockHandler} />)
+
+      const button = screen.getByText('delete')
+      await user.click(button)
+
+      expect(mockHandler.mock.calls).toHaveLength(1)
+    })
+
+    test('handler not called if user cancels', async () => {
+      jest.spyOn(globalThis, 'confirm').mockReturnValueOnce(false)
+      const mockHandler = jest.fn()
+      const user = userEvent.setup()
+
+      render(<Blog blog={blog} showRemove={true} onRemoveBlog={mockHandler} />)
+
+      const button = screen.getByText('delete')
+      await user.click(button)
+
+      expect(mockHandler.mock.calls).toHaveLength(0)
+    })
+  })
 })
