9c09804aaaac11c0df201218bd9cda32aec38e7f
dc <dc@local>
2023-12-28 14:50:56 +0000

    v: e: p1b: 1.5

--- exercises/part1/courseinfo-vanilla/src/App.js
+++ exercises/part1/courseinfo-vanilla/src/App.js
@@ -1,50 +1,56 @@
 const Header = (course) => {
     const root = document.createElement('h1');
-    root.textContent = course;
+    root.textContent = course.name;
     return root;
 };
 
-const Part = (part, exercises) => {
+const Part = (name, exercises) => {
     const root = document.createElement('p');
-    root.textContent = `${part} ${exercises}`;
+    root.textContent = `${name} ${exercises}`;
     return root;
 };
 
-const Content = (parts) => {
+const Content = (course) => {
     const root = document.createElement('div');
-    root.appendChild(Part(parts[0].name, parts[0].exercises));
-    root.appendChild(Part(parts[1].name, parts[1].exercises));
-    root.appendChild(Part(parts[2].name, parts[2].exercises));
+    root.appendChild(Part(course.parts[0].name, course.parts[0].exercises));
+    root.appendChild(Part(course.parts[1].name, course.parts[1].exercises));
+    root.appendChild(Part(course.parts[2].name, course.parts[2].exercises));
     return root;
 };
 
-const Total = (parts) => {
+const Total = (course) => {
     const root = document.createElement('p');
-    root.textContent = `Number of exercises: ${parts[0].exercises + parts[1].exercises + parts[2].exercises}`;
+    root.textContent = `Number of exercises: ${
+        course.parts[0].exercises
+        + course.parts[1].exercises
+        + course.parts[2].exercises
+    }`;
     return root;
 };
 
 const App = () => {
-    const course = 'Half Stack application development';
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10,
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7,
-        },
-        {
-            name: 'State of a component',
-            exercises: 14,
-        },
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+            },
+        ],
+    };
 
     const root = document.createElement('div');
     root.appendChild(Header(course));
-    root.appendChild(Content(parts));
-    root.appendChild(Total(parts));
+    root.appendChild(Content(course));
+    root.appendChild(Total(course));
     return root;
 };
 
