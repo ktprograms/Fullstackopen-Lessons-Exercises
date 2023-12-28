9b5f65a1db1be57383ebb7a3013da6c87ac15f11
dc <dc@local>
2023-12-28 14:05:31 +0000

    v: e: p1a: 1.1

--- exercises/part1/courseinfo-vanilla/src/App.js
+++ exercises/part1/courseinfo-vanilla/src/App.js
@@ -1,3 +1,30 @@
+const Header = (course) => {
+    const root = document.createElement('h1');
+    root.textContent = course;
+    return root;
+};
+
+const Content = (part1, exercises1, part2, exercises2, part3, exercises3) => {
+    const p_part1 = document.createElement('p');
+    p_part1.textContent = `${part1} ${exercises1}`;
+    const p_part2 = document.createElement('p');
+    p_part2.textContent = `${part2} ${exercises2}`;
+    const p_part3 = document.createElement('p');
+    p_part3.textContent = `${part3} ${exercises3}`;
+
+    const root = document.createElement('div');
+    root.appendChild(p_part1);
+    root.appendChild(p_part2);
+    root.appendChild(p_part3);
+    return root;
+};
+
+const Total = (exercises1, exercises2, exercises3) => {
+    const root = document.createElement('p');
+    root.textContent = `Number of exercises: ${exercises1 + exercises2 + exercises3}`;
+    return root;
+};
+
 const App = () => {
     const course = 'Half Stack application development';
     const part1 = 'Fundamentals of React';
@@ -7,23 +34,10 @@ const App = () => {
     const part3 = 'State of a component';
     const exercises3 = 14;
 
-    const h1_header = document.createElement('h1');
-    h1_header.textContent = course;
-    const p_part1 = document.createElement('p');
-    p_part1.textContent = `${part1} ${exercises1}`;
-    const p_part2 = document.createElement('p');
-    p_part2.textContent = `${part2} ${exercises2}`;
-    const p_part3 = document.createElement('p');
-    p_part3.textContent = `${part3} ${exercises3}`;
-    const p_total = document.createElement('p');
-    p_total.textContent = `Number of exercises: ${exercises1 + exercises2 + exercises3}`;
-
     const root = document.createElement('div');
-    root.appendChild(h1_header);
-    root.appendChild(p_part1);
-    root.appendChild(p_part2);
-    root.appendChild(p_part3);
-    root.appendChild(p_total);
+    root.appendChild(Header(course));
+    root.appendChild(Content(part1, exercises1, part2, exercises2, part3, exercises3));
+    root.appendChild(Total(exercises1, exercises2, exercises3));
     return root;
 };
 
