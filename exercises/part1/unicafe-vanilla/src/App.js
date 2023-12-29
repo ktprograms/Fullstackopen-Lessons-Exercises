fecf9545e3123d217155e9a2d155bad964ad9c86
dc <dc@local>
2023-12-29 02:26:45 +0000

    v: e p1d: setup

---
+++ exercises/part1/unicafe-vanilla/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+    const good = 0;
+    const neutral = 0;
+    const bad = 0;
+
+    const root = document.createElement('div');
+    root.textContent = 'code here';
+    return root;
+};
+
+export default App;
