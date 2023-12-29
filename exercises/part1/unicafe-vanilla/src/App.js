f5ca243321016db48a1c8fc19b972f14640fb4de
dc <dc@local>
2023-12-29 07:25:00 +0000

    v: e: p1d: 1.7

--- exercises/part1/unicafe-vanilla/src/App.js
+++ exercises/part1/unicafe-vanilla/src/App.js
@@ -3,27 +3,43 @@ const App = () => {
     let neutral = 0;
     let bad = 0;
 
+    const calculateAverage = (good, neutral, bad) => (
+        (good + (bad * -1)) / (good + neutral + bad)
+    );
+
+    const calculatePositive = (good, neutral, bad) => (
+        (good / (good + neutral + bad)) * 100
+    );
+
     const t_good = document.createTextNode(`good ${good}`);
     const t_neutral = document.createTextNode(`neutral ${neutral}`);
     const t_bad = document.createTextNode(`bad ${bad}`);
+    const t_average = document.createTextNode(`average ${calculateAverage(good, neutral, bad)}`);
+    const t_positive = document.createTextNode(`positive ${calculatePositive(good, neutral, bad)}`);
 
     const button_good = document.createElement('button');
     button_good.textContent = 'good';
     button_good.addEventListener('click', () => {
         good++;
         t_good.textContent = `good ${good}`;
+        t_average.textContent = `average ${calculateAverage(good, neutral, bad)}`;
+        t_positive.textContent = `positive ${calculatePositive(good, neutral, bad)}`;
     });
     const button_neutral = document.createElement('button');
     button_neutral.textContent = 'neutral';
     button_neutral.addEventListener('click', () => {
         neutral++;
         t_neutral.textContent = `neutral ${neutral}`;
+        t_average.textContent = `average ${calculateAverage(good, neutral, bad)}`;
+        t_positive.textContent = `positive ${calculatePositive(good, neutral, bad)}`;
     });
     const button_bad = document.createElement('button');
     button_bad.textContent = 'bad';
     button_bad.addEventListener('click', () => {
         bad++;
         t_bad.textContent = `bad ${bad}`;
+        t_average.textContent = `average ${calculateAverage(good, neutral, bad)}`;
+        t_positive.textContent = `positive ${calculatePositive(good, neutral, bad)}`;
     });
 
     const h1_giveFeedback = document.createElement('h1');
@@ -43,6 +59,10 @@ const App = () => {
     div_statistics.appendChild(t_neutral);
     div_statistics.appendChild(document.createElement('br'));
     div_statistics.appendChild(t_bad);
+    div_statistics.appendChild(document.createElement('br'));
+    div_statistics.appendChild(t_average);
+    div_statistics.appendChild(document.createElement('br'));
+    div_statistics.appendChild(t_positive);
 
     const root = document.createElement('div');
     root.appendChild(h1_giveFeedback);
