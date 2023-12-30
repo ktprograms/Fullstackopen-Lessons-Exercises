624455cc13e99691229bf036476340958e671ba5
dc <dc@local>
2023-12-30 04:36:47 +0000

    vt: e: p1b: 1.4

--- exercises/part1/courseinfo-vanilla-include/src/App.js
+++ exercises/part1/courseinfo-vanilla-include/src/App.js
@@ -20,26 +20,26 @@ const Part = (name, exercises) => {
     return root;
 };
 
-const Content = (part1, part2, part3) => {
+const Content = (parts) => {
     const template = document.querySelector('template[data-component=Content]').content.cloneNode(true);
     const root = document.createElement('div');
     root.appendChild(template);
     const render = () => {
-        root.querySelector('.content_part1').replaceChildren(Part(part1.name, part1.exercises));
-        root.querySelector('.content_part2').replaceChildren(Part(part2.name, part2.exercises));
-        root.querySelector('.content_part3').replaceChildren(Part(part3.name, part3.exercises));
+        root.querySelector('.content_part1').replaceChildren(Part(parts[0].name, parts[0].exercises));
+        root.querySelector('.content_part2').replaceChildren(Part(parts[1].name, parts[1].exercises));
+        root.querySelector('.content_part3').replaceChildren(Part(parts[2].name, parts[2].exercises));
     };
     render();
     return root;
 };
 
-const Total = (part1, part2, part3) => {
+const Total = (parts) => {
     const template = document.querySelector('template[data-component=Total]').content.cloneNode(true);
     const root = document.createElement('div');
     root.appendChild(template);
     const render = () => {
         root.querySelector('.total_total').textContent = `Number of exercises ${
-            part1.exercises + part2.exercises + part3.exercises
+            parts[0].exercises + parts[1].exercises + parts[2].exercises
         }`;
     };
     render();
@@ -48,26 +48,28 @@ const Total = (part1, part2, part3) => {
 
 const App = () => {
     const course = 'Half Stack application development';
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10,
-    };
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7,
-    };
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14,
-    };
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+        },
+        {
+            name: 'State of a component',
+            exercises: 14,
+        },
+    ];
 
     const template = document.querySelector('template[data-component=App]').content.cloneNode(true);
     const root = document.createElement('div');
     root.appendChild(template);
     const render = () => {
         root.querySelector('.app_header').replaceChildren(Header(course));
-        root.querySelector('.app_content').replaceChildren(Content(part1, part2, part3));
-        root.querySelector('.app_total').replaceChildren(Total(part1, part2, part3));
+        root.querySelector('.app_content').replaceChildren(Content(parts));
+        root.querySelector('.app_total').replaceChildren(Total(parts));
     };
     render();
     return root;
