9c0e7b7c5233383581a37e50bcaae6cf3bed6afc
dc <dc@local>
2023-12-30 09:56:03 +0000

    vt: e: p2b: 2.6

--- exercises/part2/phonebook-vanilla-include/src/App.js
+++ exercises/part2/phonebook-vanilla-include/src/App.js
@@ -1,5 +1,17 @@
+const Person = (key, person) => {
+    const template = document.querySelector('template[data-component=Person]').content.cloneNode(true);
+    const root = document.createElement('div');
+    root.appendChild(template);
+    const render = () => {
+        root.dataset.key = key;
+        root.textContent = person.name;
+    };
+    render();
+    return root;
+};
+
 const App = () => {
-    const persons = [
+    let persons = [
         {
             name: 'Arto Hellas',
         },
@@ -8,7 +20,23 @@ const App = () => {
     const template = document.querySelector('template[data-component=App]').content.cloneNode(true);
     const root = document.createElement('div');
     root.appendChild(template);
-    const render = () => {};
+
+    /** @type {HTMLFormElement} */ const form = root.querySelector('.app_form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const name = formData.get('name');
+        persons.push({ name });
+        form.reset();
+        render();
+    });
+
+    const render = () => {
+        root.querySelector('.app_persons').replaceChildren(
+            ...persons.map((person) => Person(person.name, person))
+        );
+    };
     render();
     return root;
 };
