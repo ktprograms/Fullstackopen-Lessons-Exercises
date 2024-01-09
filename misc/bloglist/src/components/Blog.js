bb6371bbc0594c353e3042662b1f10880c90bf74
dc <dc@local>
2024-01-09 08:49:29 +0000

    misc: bloglist: details
    using re-render all

--- misc/bloglist/src/components/Blog.js
+++ misc/bloglist/src/components/Blog.js
@@ -12,7 +12,7 @@ export const Blog = {
         Blog.$.form.addEventListener('submit', function(event) {
             event.preventDefault();
 
-            const formData = new FormData(Blog.$.form);
+            const formData = new FormData(event.target);
             const title = formData.get('title').trim();
             const author = formData.get('author').trim();
             const url = formData.get('url').trim();
@@ -23,22 +23,48 @@ export const Blog = {
                 });
             }
 
-            Blog.$.form.reset();
+            event.target.reset();
         });
+        Blog.bindItemEvents();
         Blog.render();
+    },
+
+    bindItemEvents() {
+        Blog.$.list.addEventListener('click', function(event) {
+            if (event.target.matches('[data-component="details"]')) {
+                const el = event.target.closest('[data-id]');
+                Blog.Model.toggleDetails(el.dataset.id);
+            }
+        });
     },
 
     createItem(blog) {
         const el = document.querySelector('#blog').content.cloneNode(true);
+        el.querySelector('.blog').dataset.id = blog.id;
+
         el.querySelector('[data-component="title"]').textContent = blog.title;
         el.querySelector('[data-component="author"]').textContent = blog.author;
+
+        if (blog.detailsShown) {
+            el.querySelector('[data-component="details"]').textContent = 'hide';
+            el.querySelector('[data-component="details-div"]').classList.remove('hidden');
+        } else {
+            el.querySelector('[data-component="details"]').textContent = 'show';
+            el.querySelector('[data-component="details-div"]').classList.add('hidden');
+        }
+
+        el.querySelector('[data-component="url"]').textContent = blog.url;
+        el.querySelector('[data-component="likes"]').textContent = blog.likes;
+
         return el;
     },
     render() {
         Blog.$.list.replaceChildren(
-            ...Blog.Model
-                .all()
-                .map(Blog.createItem)
+            ...(
+                Blog.Model
+                    .all()
+                    .map(Blog.createItem)
+            )
         );
     },
 };
--- misc/bloglist/src/models/Blog.model.js
+++ misc/bloglist/src/models/Blog.model.js
@@ -7,40 +7,72 @@ export const BlogModel = class extends EventTarget {
                 author: 'Michael Chan',
                 url: 'https://reactpatterns.com/',
                 likes: 7,
+                id: '0',
             },
             {
                 title: 'Go To Statement Considered Harmful',
                 author: 'Edsger W. Dijkstra',
                 url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
                 likes: 5,
+                id: '1',
             },
             {
                 title: 'Canonical string reduction',
                 author: 'Edsger W. Dijkstra',
                 url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
                 likes: 12,
+                id: '2',
             },
             {
                 title: 'First class tests',
                 author: 'Robert C. Martin',
                 url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
                 likes: 10,
+                id: '3',
             },
             {
                 title: 'TDD harms architecture',
                 author: 'Robert C. Martin',
                 url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
                 likes: 0,
+                id: '4',
             },
         ];
     }
 
+    #save() {
+        this.dispatchEvent(new CustomEvent('save'));
+    }
+
+    // Getters
     all() {
         return this.blogs;
     }
 
+    // Backend operations (CRUD)
     create(blog) {
-        this.blogs.push(blog);
-        this.dispatchEvent(new CustomEvent('save'));
+        const id = String(
+            Math.max(
+                ...(
+                    this.blogs.map(function(blog) {
+                        return blog.id;
+                    })
+                )
+            ) + 1
+        );
+        this.blogs.push({ ...blog, id });
+        this.#save();
+    }
+
+    // Frontend state
+    toggleDetails(id) {
+        this.blogs = this.blogs.map(function(blog) {
+            if (blog.id === id) {
+                return { ...blog, detailsShown: !blog.detailsShown };
+            } else {
+                return blog;
+            }
+        });
+        this.#save();
     }
 };
