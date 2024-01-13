848c285a0561d5e4bb649a5fad0fbb4e76433688
dc <dc@local>
2024-01-13 13:02:02 +0000

    c: e: p1: courseinfo: 1: setup
    setup: initial courseinfo data

---
+++ curriculum/exercises/part1/courseinfo/src/main.js
@@ -0,0 +1,9 @@
+var course = 'Half Stack application development';
+var part1 = 'Fundamentals of React';
+var exercises1 = 10;
+var part2 = 'Using props to pass data';
+var exercises2 = 7;
+var part3 = 'State of a component';
+var exercises3 = 14;
+
+document.querySelector('#root').textContent = 'Hello, world!';
