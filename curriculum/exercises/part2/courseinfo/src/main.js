850320877fd87436d37b6a1d35a42c178450ea92
dc <dc@local>
2024-01-16 02:42:51 +0000

    c: e: p2: courseinfo: 1: setup
    setup: copy project from p1 courseinfo

---
+++ curriculum/exercises/part2/courseinfo/src/main.js
@@ -0,0 +1,23 @@
+const course = {
+    name: 'Half Stack application development',
+    parts: [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10,
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7,
+        },
+        {
+            name: 'State of a component',
+            exercises: 14,
+        },
+    ],
+};
+
+document.querySelector('#header').textContent = course.name;
+document.querySelector('#part1').textContent = course.parts[0].name + ' ' + course.parts[0].exercises;
+document.querySelector('#part2').textContent = course.parts[1].name + ' ' + course.parts[1].exercises;
+document.querySelector('#part3').textContent = course.parts[2].name + ' ' + course.parts[2].exercises;
+document.querySelector('#total').textContent = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
